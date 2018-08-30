1fe47cc2d3bd927b10b208e438284697032dfc72
Alkaidek <35803025+Alkaidek@users.noreply.github.com>
2018-08-30 12:54:24 +0200

    v13.46

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material';
 
 const firebase = {
-  apiKey: 'AIzaSyB7P4-5PpvWM1D5dZ6rIeNRlczTt1nlBS4',
-  authDomain: 'diploma-93bc1.firebaseapp.com',
-  databaseURL: 'https://diploma-93bc1.firebaseio.com',
-  projectId: 'diploma-93bc1',
-  storageBucket: 'diploma-93bc1.appspot.com',
-  messagingSenderId: '739562136666'
+  apiKey: 'AIzaSyB2EVW_LFNCbihRQ29rZTYtYLT_Ni_AvT8',
+  authDomain: 'dyplomator.firebaseapp.com',
+  databaseURL: 'https://dyplomator.firebaseio.com',
+  projectId: 'dyplomator',
+  storageBucket: 'dyplomator.appspot.com',
+  messagingSenderId: '633386395085'
 }
 
 @NgModule({
